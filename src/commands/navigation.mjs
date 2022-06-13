42b4f3201cd62cf4b634c55dabf62a617acc6cc0
Aliaksei Zhuk <aliaksei_zhuk@epam.com>
2022-06-13 12:36:26 +0400

    fix: argument in cd function

--- src/commands/navigation.mjs
+++ src/commands/navigation.mjs
@@ -1,14 +1,16 @@
 import fs from 'fs';
 import { homedir } from 'os';
+import path from 'path';
 import { printError } from '../helpers.mjs';
 
 export const up = () => {
   if (homedir() !== process.cwd()) process.chdir('../');
 };
 
-export const cd = (args) => {
+export const cd = (newPath) => {
+  const fullPath = path.resolve(process.cwd(), newPath);
   try {
-    process.chdir(args[0]);
+    process.chdir(fullPath);
   } catch (e) {
     printError();
   }
